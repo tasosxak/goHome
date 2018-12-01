aefc41e2c5ff768841d0e06b5243422f5a5a8e91
spyridon97 <spyridon97@hotmail.com>
2018-12-01 17:09:11 +0200

    makis

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -13,34 +13,65 @@ import {
 import { MonoText } from '../components/StyledText';
 import getDirections from 'react-native-google-maps-directions';
 
+import { Constants, Location, Permissions } from 'expo';
+
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
 
-   handleGetDirections = () => {
-      const data = {
-         source: {
-          latitude: -33.8356372,
-          longitude: 18.6947617
-        },
-        destination: {
-          latitude: -33.8600024,
-          longitude: 18.697459
-        },
-        params: [
-          {
-            key: "travelmode",
-            value: "driving"        // may be "driving", "walking", "bicycling" or "transit" as well
-          },
-          {
-            key: "dir_action",
-            value: "navigate"       // this instantly initializes navigation using the given travel mode
-          }
-        ]
-      }
 
-      getDirections(data)
+    _getLocationAsync = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+            this.setState({
+                errorMessage: 'Permission to access location was denied',
+            });
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        this.setState({ location });
+    };
+
+
+    handleGetDirections = async () => {
+
+        var {status} = await Permissions.askAsync(Permissions.LOCATION)
+
+        if (status !== 'granted') {
+            alert('Permission to access location was denied');
+        } else  {
+
+            var location = await Location.getCurrentPositionAsync({});
+
+            var long = location.coords.longitude;
+            var lat = location.coords.latitude;
+
+            const data = {
+                source: {
+                    latitude: lat,
+                    longitude: long
+                },
+                destination: {
+                    latitude: -33.8600024,
+                    longitude: 18.697459
+                },
+                params: [
+                    {
+                        key: "travelmode",
+                        value: "walking"        // may be "driving", "walking", "bicycling" or "transit" as well
+                    },
+                    {
+                        key: "dir_action",
+                        value: "navigate"       // this instantly initializes navigation using the given travel mode
+                    }
+                ]
+            }
+
+            getDirections(data)
+            alert(data);
+
+        }
     }
 
 
