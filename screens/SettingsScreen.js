e7f8ba906bf383320b7fdb3a4e9855d609a8cae8
tasosxak <tasosxak@hotmail.com>
2018-12-01 16:40:51 +0200

    ggg

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
@@ -9,6 +8,6 @@ export default class SettingsScreen extends React.Component {
   render() {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return <View></View>;
   }
 }
